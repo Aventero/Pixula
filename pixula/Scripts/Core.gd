5dd4b17b2737ff4b33fcbfffd2bdf6658e36e4d2
Henrik Weidner <henrikweidner98@gmail.com>
2025-01-01 03:48:36 +0100

    Improved overall Speed

--- pixula/Scripts/Core.gd
+++ pixula/Scripts/Core.gd
@@ -5,15 +5,19 @@ extends Node2D
 @export var camera: Camera2D
 @onready var pixel_size = camera.zoom.x
 
-var pixel_array_2d: Array[Array] = []
+var pixels: Array[Array] = []
+var active_pixels: Dictionary = {}
+var next_active_pixels: Dictionary = {}
+
 @onready var width: int = get_viewport_rect().size.x / pixel_size
 @onready var height: int = get_viewport_rect().size.y / pixel_size
 
-const PROCESSED_BIT = 0
+const PROCESSED_BIT_START = 0
+const ACTIVE_BIT_START = 1
 const MATERIAL_BITS_START = 5
-const MATERIAL_BITS_MASK = 0b11111 # 5 Bit = 32
-const VARIANT_BITS_START = 12
-const VARIANT_BITS_MASK = 0b1111111 # 7 Bit
+const MATERIAL_BITS_MASK = 0b1111 # 4 Bit = 16 materials
+const VARIANT_BITS_START = 13
+const VARIANT_BITS_MASK = 0b1111111 # 7 Bit "of color"
 
 # 0 - 31 -> 32 Possible Materials (Material Space)
 enum MaterialType {
@@ -21,13 +25,15 @@ enum MaterialType {
 	SAND = 1,
 	WATER = 2,
 	STONE = 3,
+	DEBUG = 4
 }
 
 const COLOR_RANGES = {
 	MaterialType.AIR: [36, 37],
 	MaterialType.SAND: [19, 23],
 	MaterialType.WATER: [1, 5],
-	MaterialType.STONE: [3, 3]
+	MaterialType.STONE: [3, 3],
+	MaterialType.DEBUG: [27, 27],
 }
 
 const SWAP_RULES = {
@@ -38,131 +44,144 @@ const SWAP_RULES = {
 }
 
 func _ready() -> void:
+	get_window().size = Vector2(width * pixel_size, height * pixel_size)
 	setup_pixels()
 
 func _on_timer_timeout() -> void:
-	var sand_count = 0
-	for y in range(0, height):
-		for x in range(0, width):
-			simulate(x, y)
+	simulate_active()
 
-	var water_count = 0
-	for y in range(0, height):
-		for x in range(0, width):
-			if get_process_material_at(x, y) == MaterialType.WATER:
-				water_count += 1
-	print("Water: ", water_count)
-	water_count = 0
-	transfer_to_tilemap()
+func simulate_active() -> void:
+	for pos in active_pixels:
+		simulate(pos.x, pos.y)
+
+	# Move next frame
+	active_pixels = next_active_pixels.duplicate()
+	next_active_pixels.clear()
 
 ### Setup, Input
 func setup_pixels() -> void:
-	pixel_array_2d.resize(height)
+	pixels.resize(height)
 	for y in height:
-		pixel_array_2d[y] = [] # new array
-		pixel_array_2d[y].resize(width) # make space
+		pixels[y] = [] # new array
+		pixels[y].resize(width) # make space
 		for x in width:
-			set_pixel_state_at(x, y, MaterialType.AIR, get_random_variant(MaterialType.AIR))
+			set_state_at(x, y, MaterialType.AIR, get_random_variant(MaterialType.AIR), false, false)
+
+	# First Draw
+	for y in range(height):
+		for x in range(width):
+			var mat_variant = get_pixel_variant_at(x, y)
+			map.set_cell(Vector2i(x, y), 1, Vector2i(mat_variant, 0), 0)
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_released("SPAWN_SAND") || event.is_action_released("SPAWN_WATER"):
 		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 
-	#if event.is_action_pressed("CHECK_MATERIAL"):
-		#var atlas_coord = map.get_cell_atlas_coords(get_mouse_tile_pos())
-		#print(atlas_to_material(atlas_coord))
+	if event.is_action_released("STATS"):
+		print("Active: ", active_pixels.size())
+		print("Active NEXT FRAME: ", next_active_pixels.size())
 
 func _process(_delta: float) -> void:
 	if Input.is_action_pressed("SPAWN_SAND"):
 		Input.mouse_mode = Input.MOUSE_MODE_HIDDEN
-		spawn_in_radius(get_mouse_tile_pos().x, get_mouse_tile_pos().y, 2, MaterialType.SAND)
+		spawn_in_radius(get_mouse_tile_pos().x, get_mouse_tile_pos().y, 1, MaterialType.SAND)
 
 	if Input.is_action_pressed("SPAWN_WATER"):
 		Input.mouse_mode = Input.MOUSE_MODE_HIDDEN
-		spawn_in_radius(get_mouse_tile_pos().x, get_mouse_tile_pos().y, 3, MaterialType.WATER)
+		spawn_in_radius(get_mouse_tile_pos().x, get_mouse_tile_pos().y, 2, MaterialType.WATER)
 
-func spawn_in_radius(center_x: int, center_y: int, radius: int, material: MaterialType) -> void:
+func spawn_in_radius(center_x: int, center_y: int, radius: int, material_type: MaterialType) -> void:
 	for y in range(max(0, center_y - radius), min(height, center_y + radius + 1)):
 		for x in range(max(0, center_x - radius), min(width, center_x + radius + 1)):
 			if Vector2(center_x, center_y).distance_to(Vector2(x, y)) <= radius:
-				set_pixel_state_at(x, y, material, get_random_variant(material))
+				set_state_at(x, y, material_type, get_random_variant(material_type), false, true)
 
-func set_pixel_state_at(x: int, y: int, material_type: MaterialType, variant: int, has_processed: bool = false) -> void:
+### Low level pixel manipulation ###
+func set_state_at(x: int, y: int, material_type: MaterialType, variant: int, has_processed: bool = false, activate: bool = false) -> void:
 	if not is_valid_position(x,y):
 		return
 
-	pixel_array_2d[y][x] = ((has_processed as int) << PROCESSED_BIT) | \
+	# the "\" is for allowing linebreaks
+	pixels[y][x] = ((has_processed as int) << PROCESSED_BIT_START) | \
 							(material_type << MATERIAL_BITS_START) | \
 							(variant << VARIANT_BITS_START)
+	set_active_at(x, y, activate)
+	draw_pixel_at(x, y)
 
-func set_pixel_processed_at(x: int, y: int, has_processed: bool) -> void:
-	pixel_array_2d[y][x] = (pixel_array_2d[y][x] & ~1) | (has_processed as int) # clear first bit and set it
+func set_active_at(x: int, y: int, active: bool) -> void:
+	var pos = Vector2i(x, y)
+	if active:
+		next_active_pixels[pos] = true
+	else:
+		next_active_pixels.erase(pos)
 
-func get_process_material_at(x: int, y: int) -> MaterialType:
-	return (pixel_array_2d[y][x] >> MATERIAL_BITS_START) & MATERIAL_BITS_MASK as MaterialType
+	# Update the bit in pixel data
+	pixels[y][x] = (pixels[y][x] & ~(1 << ACTIVE_BIT_START)) | ((active as int) << ACTIVE_BIT_START)
+
+func set_processed_at(x: int, y: int, has_processed: bool) -> void:
+	pixels[y][x] = (pixels[y][x] & ~1) | (has_processed as int)
+
+func get_material_at(x: int, y: int) -> MaterialType:
+	return (pixels[y][x] >> MATERIAL_BITS_START) & MATERIAL_BITS_MASK as MaterialType
 
 func get_pixel_variant_at(x: int, y: int) -> int:
-	return (pixel_array_2d[y][x] >> VARIANT_BITS_START) & VARIANT_BITS_MASK
+	return (pixels[y][x] >> VARIANT_BITS_START) & VARIANT_BITS_MASK
 
-func is_pixel_processed_at(x: int, y: int) -> bool:
-	return (pixel_array_2d[y][x] >> PROCESSED_BIT) & 0x1
+func is_processed_at(x: int, y: int) -> bool:
+	return (pixels[y][x] >> PROCESSED_BIT_START) & 0b1
+
+func is_active_at(x: int, y: int) -> bool:
+	return (pixels[y][x] >> ACTIVE_BIT_START) & 0b1
 
 func get_random_variant(material_type: MaterialType) -> int:
 	var variants = COLOR_RANGES[material_type]
 	return randi_range(variants[0], variants[1])
 
-func transfer_to_tilemap() -> void:
-	for y in range(height):
-		for x in range(width):
-			var mat_variant = get_pixel_variant_at(x, y)
-			map.set_cell(Vector2i(x, y), 1, Vector2i(mat_variant, 0), 0)
-			set_pixel_processed_at(x, y, false)
-
-func is_valid_position(x: int, y: int) -> bool:
-	return x >= 0 and x < width and y >= 0 and y < height
-
 ### Mechanics
 func simulate(x: int, y: int) -> void:
-	if is_pixel_processed_at(x, y):
-		return
+	var current_material: MaterialType = get_material_at(x, y)
 
-	var current_material: MaterialType = get_process_material_at(x, y)
-
-	# Is some sand material
 	if current_material == MaterialType.SAND:
 		sand_mechanic(x, y, current_material)
 
-	# Is some water material
 	if current_material == MaterialType.WATER:
 		water_mechanic(x, y, current_material)
 
-	set_pixel_processed_at(x, y, true) # Mark as processed
-
 func sand_mechanic(x: int, y: int, process_material: MaterialType) -> void:
 	if not move_down(x, y, process_material):
-		move_diagonal(x, y, process_material)
+		if not move_diagonal(x, y, process_material):
+			set_active_at(x, y, false)
 
 func water_mechanic(x: int, y: int, process_material: MaterialType) -> void:
 	if not move_down(x, y, process_material):
 		if not move_diagonal(x, y, process_material):
-			move_horizontal(x, y, process_material)
+			if not move_horizontal(x, y, process_material):
+				set_active_at(x, y, false)
 
 func move_horizontal(x: int, y: int, process_material: MaterialType) -> bool:
 	var x_direction: int = 1 if y % 2 == 0 else -1
-	if not is_valid_position(x + x_direction, y):
+	var new_x = x + x_direction
+	if not is_valid_position(new_x, y):
 		return false
 
-	if can_swap(process_material, get_process_material_at(x + x_direction, y)):
-		swap_particle(x, y, x + x_direction, y)
+	if can_swap(process_material, get_material_at(new_x, y)):
+		swap_particle(x, y, new_x, y)
 		return true
 
-	return false
+	new_x += x_direction * 2
+	if not is_valid_position(new_x, y):
+		return false
+	if can_swap(process_material, get_material_at(new_x, y)):
+		swap_particle(x, y, new_x, y)
+		return true
+
+	return true
 
 func move_down(x: int, y: int, process_material: MaterialType) -> bool:
 	if not is_valid_position(x, y + 1):
 		return false
 
-	if can_swap(process_material, get_process_material_at(x, y + 1)):
+	if can_swap(process_material, get_material_at(x, y + 1)):
 		swap_particle(x, y, x, y + 1)
 		return true
 
@@ -175,27 +194,44 @@ func move_diagonal(x: int, y: int, process_material: MaterialType) -> bool:
 	if not is_valid_position(new_x, y + 1):
 		return false
 
-	if can_swap(process_material, get_process_material_at(new_x, y + 1)):
+	if can_swap(process_material, get_material_at(new_x, y + 1)):
 		swap_particle(x, y, new_x, y + 1)
 		return true
 
 	return false
 
+func activate_surrounding_pixels(x: int, y: int) -> void:
+	for y_direction in range(-1, 2):
+		for x_direction in range(-1, 2):
+			var activate_pos_x = x + x_direction
+			var activate_pos_y = y + y_direction
+			if not is_valid_position(activate_pos_x, activate_pos_y):
+				continue
+			if get_material_at(activate_pos_x, activate_pos_y) != MaterialType.AIR:
+				set_active_at(activate_pos_x, activate_pos_y, true)
+
+func draw_pixel_at(x: int, y: int) -> void:
+	var mat_variant = get_pixel_variant_at(x, y)
+	map.set_cell(Vector2i(x, y), 1, Vector2i(mat_variant, 0), 0)
+
+func swap_particle(source_x: int, source_y: int, destination_x: int, destination_y: int) -> void:
+	# Swap the state
+	var tmp = pixels[destination_y][destination_x]
+	pixels[destination_y][destination_x] = pixels[source_y][source_x]
+	pixels[source_y][source_x] = tmp
+
+	# Draw only the changed cells
+	draw_pixel_at(source_x, source_y)
+	draw_pixel_at(destination_x, destination_y)
+
+	activate_surrounding_pixels(destination_x, destination_y)
+	activate_surrounding_pixels(source_x, source_y)
+
 func can_swap(source: MaterialType, swapping_partner: MaterialType) -> bool:
 	return swapping_partner in SWAP_RULES.get(source, [])
 
-func swap_particle(source_x: int, source_y: int, destination_x: int, destination_y: int) -> void:
-	if not can_swap(get_process_material_at(source_x, source_y), get_process_material_at(destination_x, destination_y)):
-		return
-
-	var tmp = pixel_array_2d[destination_y][destination_x]
-	pixel_array_2d[destination_y][destination_x] = pixel_array_2d[source_y][source_x]
-	pixel_array_2d[source_y][source_x] = tmp
-
-	# The destination pixel is processed, otherwise it would get re-processed
-	set_pixel_processed_at(destination_x, destination_y, true)
-	set_pixel_processed_at(source_x, source_y, false)
-### Helper
+func is_valid_position(x: int, y: int) -> bool:
+	return x >= 0 and x < width and y >= 0 and y < height
 
 func get_mouse_tile_pos() -> Vector2i:
 	var tilemap_pos = map.local_to_map(get_local_mouse_position()).abs()
