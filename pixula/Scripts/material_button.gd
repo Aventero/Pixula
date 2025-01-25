cbfd66ee75e7bc7a66cab19fe16efddded5dff35
Henrik Weidner <henrikweidner98@gmail.com>
2025-01-25 23:51:04 +0100

    Add Unique Speed Wobble to Buttons toggled

--- pixula/Scripts/material_button.gd
+++ pixula/Scripts/material_button.gd
@@ -4,5 +4,16 @@ extends Button
 signal material_pressed(type : MainEntry.MaterialType)
 @export var material_type: MainEntry.MaterialType = MainEntry.MaterialType.AIR
 
+func _ready() -> void:
+	material = material.duplicate() # Make it unique
+	toggled.connect(_on_toggled)
+
 func _pressed() -> void:
 	material_pressed.emit(material_type)
+
+func _on_toggled(button_pressed: bool):
+	# De selected
+	if !button_pressed:
+		material.set_shader_parameter("speed_multiplier", 1.0)
+	else:
+		material.set_shader_parameter("speed_multiplier", 5.0)
